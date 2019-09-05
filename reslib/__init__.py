9900388f8232306b051c9cd370a8e3e9c078739e
Mac Gaulin <gaulinmp@gmail.com>
2019-09-05 01:40:45 -0600

    Add default Logging handler

--- reslib/__init__.py
+++ reslib/__init__.py
@@ -14,3 +14,8 @@ This package provides code to facilitate common Accounting/Finance research task
 from .__version__ import (__title__, __description__, __url__, __version__,
                           __build__, __author__, __author_email__,
                           __license__, __copyright__)
+
+
+# Add NullHandler default logging handler because Requests says so.
+import logging as __log
+__log.getLogger(__name__).addHandler(__log.NullHandler())
